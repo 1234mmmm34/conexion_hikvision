273eee31b8761b349e044bfaccc270d7fe0774d8
Mariana <urquidymariana@gmail.com>
2024-05-11 17:42:41 -0700

    Agregar archivos de proyecto.

---
+++ conexion_hikvision/Controllers/ConexionController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using CardManagement;
+
+namespace conexion_hikvision.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConexionController : ControllerBase
+    {
+        [HttpGet]
+        [Route("login")]
+
+
+        public bool login()
+        {
+            return AddDevice.Login("admin", "Repara123", "5551", "187.216.118.73");
+
+
+        }
+    }
+}
